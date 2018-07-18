0ba3f0f203d84408523f265303705bdf12ecb79f
Andrew <afuselier23@gmail.com>
2018-07-18 10:30:24 -0700

    add blocking code definition and call inside GET /

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const express = require('express');
+
 const app = express();
 
+// Mock function to slow down server
+function doWork(duration) {
+  const start = Date.now();
+  while (Date.now() - start < duration) {}
+}
+
 app.get('/', (req, res) => {
+  // Blocking the event loop
+  doWork(5000);
+
   res.send('Hello World!');
 });
 
