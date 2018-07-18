f0f6bc88517c2b1bc05555b0e95dda0591f156a0
Andrew <afuselier23@gmail.com>
2018-07-18 10:39:43 -0700

    adding clustering

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
-const express = require('express');
+const cluster = require('cluster');
 
-const app = express();
+// Check if the file is being executed on the master
+if (cluster.isMaster) {
+  // Execute the index file in child mode
+  cluster.fork();
+  cluster.fork();
+  cluster.fork();
+} else {
 
-// Mock function to slow down server
-function doWork(duration) {
-  const start = Date.now();
-  while (Date.now() - start < duration) {}
+  const express = require('express');
+  const app = express();
+  
+  // Mock function to slow down server
+  function doWork(duration) {
+    const start = Date.now();
+    while (Date.now() - start < duration) {}
+  }
+
+  app.get('/', (req, res) => {
+    // Blocking the event loop
+    doWork(5000);
+    res.send('Hello World!');
+  });
+
+  app.get('/fast', (req, res) => {
+    res.send('Fast!');
+  }); 
+  app.listen(3000);
 }
 
-app.get('/', (req, res) => {
-  // Blocking the event loop
-  doWork(5000);
 
-  res.send('Hello World!');
-});
-
-app.listen(3000);
